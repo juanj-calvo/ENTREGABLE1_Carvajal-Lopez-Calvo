da485e7953f5e5a63d003e0ec1b90af7c0fa4b97
juan calvo <juanj.c.v.01@gmail.com>
2022-08-09 23:19:22 -0300

    models

--- proyecto_coder/proyecto_app/models.py
+++ proyecto_coder/proyecto_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Celphone(models.Model):
+    celphone_name = models.CharField(max_length=40)
+    celphone_id =models.FloatField
+    celphone_price = models.FloatField()
+    celphone_brand = models.CharField(max_length=40)
+    celphone_description = models.CharField(max_length=200, null=True, blank=True)
+    celphone_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100,null = True)
+
+
+class blog(models.Model):
+    blog_title = models.CharField(max_length=40)
+    blog_description = models.CharField(max_length=200)
+    blog_date = models.DateField(auto_now_add= True, null= True, blank=True)
+    blog_author = models.CharField(max_length=40)
+    
+
+class user(models.Model):
+    user_name = models.CharField(max_length=40)
+    user_email = models.CharField(max_length=60)
+    user_password = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=40)
+    user_role=models.CharField(max_length=40
